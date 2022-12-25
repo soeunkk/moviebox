51896c290b3154113d48811f46a092d6c1b94786
soeunkk <luckyber1@naver.com>
2022-12-25 22:41:15 +0900

    Initial Project
    application-aws.properties는 db 계정 정보가 담겨 있으므로 .gitignore에 추가
    구글 코드 컨벤션 적용

---
+++ src/main/java/com/example/moviebox/MovieBoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/moviebox/MovieBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
