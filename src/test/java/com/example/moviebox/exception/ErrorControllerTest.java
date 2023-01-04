da4abc00ad5863d9827d8bc6e35b6b749162e075
soeunkk <luckyber1@naver.com>
2023-01-04 21:44:35 +0900

    [test] 자체 예외 및 Http Request 에러 핸들링 (#5)

---
+++ src/test/java/com/example/moviebox/exception/ErrorControllerTest.java
@@ -0,0 +1,87 @@
+package com.example.moviebox.exception;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import com.example.moviebox.BaseControllerTest;
+import com.example.moviebox.admin.dto.AdminRequest;
+import com.example.moviebox.admin.service.AdminService;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+
+class ErrorControllerTest extends BaseControllerTest {
+	@MockBean
+	private AdminService adminService;
+
+	@DisplayName("이메일 인증 키가 올바르지 않은 경우를 에러 핸들링한다.")
+	@Test
+	public void handleBusinessException() throws Exception {
+		willThrow(BusinessException.EMAIL_AUTH_KEY_INVALID)
+			.given(adminService).emailAuth(anyString());
+
+		mockMvc.perform(get("/api/email-auth?id=kkk"))
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.type").value("COMMON-001"))
+			.andExpect(jsonPath("$.status").value(400))
+			.andExpect(jsonPath("$.instance").value("/api/email-auth"));
+	}
+
+	@DisplayName("잘못된 HTTP 메소드로 요청한 경우를 에러 핸들링한다.")
+	@Test
+	public void handleHttpRequestMethodNotSupportedException() throws Exception {
+		mockMvc.perform(post("/api/email-auth?id=kkk"))
+			.andExpect(status().isMethodNotAllowed());
+	}
+
+	@DisplayName("Parameter가 누락된 경우를 에러 핸들링한다.")
+	@Test
+	public void handleMissingServletRequestParameterException() throws Exception {
+		mockMvc.perform(get("/api/email-auth"))
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.type").value("COMMON-001"))
+			.andExpect(jsonPath("$.status").value(400))
+			.andExpect(jsonPath("$.instance").value("/api/email-auth"));
+	}
+
+	@DisplayName("Content Type이 잘못 되었을 경우를 에러 핸들링한다.")
+	@Test
+	public void handleHttpMediaTypeNotSupportedException() throws Exception {
+		mockMvc.perform(post("/api/login")
+				.contentType(MediaType.TEXT_PLAIN)
+				.content(objectMapper.writeValueAsString(
+					new AdminRequest("abc123@gmail.com", "123123")
+				)))
+			.andExpect(status().isUnsupportedMediaType())
+			.andExpect(jsonPath("$.type").value("COMMON-003"))
+			.andExpect(jsonPath("$.status").value(415))
+			.andExpect(jsonPath("$.instance").value("/api/login"));
+	}
+
+	@DisplayName("Request Body 중 일부가 누락 되었을 경우를 에러 핸들링한다.")
+	@Test
+	public void handleMethodArgumentNotValidException() throws Exception {
+		mockMvc.perform(post("/api/login")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(
+					new AdminRequest("abc123@gmail.com", null)
+				)))
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.type").value("COMMON-001"))
+			.andExpect(jsonPath("$.status").value(400))
+			.andExpect(jsonPath("$.instance").value("/api/login"));
+	}
+
+	@DisplayName("Request Body가 전체 누락 되었을 경우를 에러 핸들링한다.")
+	@Test
+	public void handleInvalidInputValueException_HttpMessageNotReadableException() throws Exception {
+		mockMvc.perform(post("/api/login")
+				.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.type").value("COMMON-001"))
+			.andExpect(jsonPath("$.status").value(400))
+			.andExpect(jsonPath("$.instance").value("/api/login"));
+	}
+}
