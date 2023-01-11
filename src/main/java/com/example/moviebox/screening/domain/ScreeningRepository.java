ad8c2f9f4b4fd792f7c1cdb43c1181d2d800b54d
soeunkk <luckyber1@naver.com>
2023-01-12 00:19:11 +0900

    [feat] 상영(Screening) 도메인 생성 (#9)
    - [chore] mysql에 json 형태로 저장하기 위해 hibernate-types-52 라이브러리 추가

---
+++ src/main/java/com/example/moviebox/screening/domain/ScreeningRepository.java
@@ -0,0 +1,7 @@
+package com.example.moviebox.screening.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScreeningRepository extends JpaRepository<Screening, Long> {
+
+}
